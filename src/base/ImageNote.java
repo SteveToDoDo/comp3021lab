076a82b6f0613929ca5d05ef4b1d3540f1b8f669
Steve To <tokin828@hotmail.com>
2022-02-23 18:00:06 +0800

    lab2

---
+++ src/base/ImageNote.java
@@ -0,0 +1,14 @@
+package base;
+
+import java.nio.file.Files;
+
+
+public class ImageNote extends Note {
+	
+	public Files image;
+	
+	public ImageNote(String title){
+		super(title);
+	}
+	
+}
--- src/comp3021lab/HelloWorld.java
+++ src/comp3021lab/HelloWorld.java
@@ -1,7 +1,7 @@
 package comp3021lab;
 
 public class HelloWorld {
-    public    static void main(String[] args)
+    public static void main(String[] args)
     {
         System.out.println("Hellow World");
     }
