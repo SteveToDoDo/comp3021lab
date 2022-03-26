03538d696ec231c436d59a31beb018c0307a71db
Steve To <tokin828@hotmail.com>
2022-03-26 13:26:39 +0800

    lab5(2)

--- src/comp3021lab/HelloWorld.java
+++ src/comp3021lab/HelloWorld.java
@@ -3,6 +3,6 @@ package comp3021lab;
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("Hellow World");
+        System.out.println((0-1)/2);
     }
 }
