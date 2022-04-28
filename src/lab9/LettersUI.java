b26815b895a58ef9a86e0c402d57c24696d29a0a
Steve To <tokin828@hotmail.com>
2022-04-28 20:07:39 +0800

    lab9(2)

--- src/lab9/LettersUI.java
+++ src/lab9/LettersUI.java
@@ -1,6 +1,7 @@
 package lab9;
 
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -15,6 +16,7 @@ public class LettersUI extends Application {
 
 	final static int SCENE_WIDTH = 300;
 	final static int SCENE_HEIGHT = 150;
+	boolean display = true;
 
 	public static void main(String[] args) {
 		launch(LettersUI.class, args);
@@ -22,11 +24,12 @@ public class LettersUI extends Application {
 
 	@Override
 	public void start(Stage stage) {
+
 		HBox hbox = new HBox();
 		hbox.setPadding(new Insets(15, 12, 15, 12));
 		hbox.setSpacing(10); // Gap between nodes
 
-		String[] string_thread = { "A", "B", "C" };
+		String[] string_thread = { "A", "B", "C", "D" ,"E"};
 		for (String s : string_thread) {
 			Text text = new Text(s);
 			text.setFont(new Font(50));
@@ -40,24 +43,45 @@ public class LettersUI extends Application {
 		stage.show();
 
 		// Since now you have to add 3 more Threads, Can we use a for loop instead ?
-		Thread t1 = new Thread(new MyTask((Text) hbox.getChildren().get(0), this));
-		Thread t2 = new Thread(new MyTask((Text) hbox.getChildren().get(1), this));
-		Thread t3 = new Thread(new MyTask((Text) hbox.getChildren().get(2), this));
+		Thread t1 = new Thread(new MyTask((Text) hbox.getChildren().get(0), this,lock));
+		Thread t2 = new Thread(new MyTask((Text) hbox.getChildren().get(1), this,lock));
+		Thread t3 = new Thread(new MyTask((Text) hbox.getChildren().get(2), this,lock));
+		Thread t4 = new Thread(new MyTask((Text) hbox.getChildren().get(3), this,lock));
+		Thread t5 = new Thread(new MyTask((Text) hbox.getChildren().get(4), this,lock));
 		t1.setDaemon(true);
 		t2.setDaemon(true);
 		t3.setDaemon(true);
+		t4.setDaemon(true);
+		t5.setDaemon(true);
 		t1.start();
 		t2.start();
 		t3.start();
+		t4.start();
+		t5.start();
 	}
 
 	public void showText(Text text, boolean show) {
 		// the parameter show tells if the text has to appear o disappear
 		if (show) {
-			text.setVisible(true);
-		} else {
-			text.setVisible(false);
-		}
+            synchronized (this) {
+                while(!display){
+                    try{
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                text.setVisible(true);
+                display = false;
+            }
+        } else {
+            synchronized (this) {
+                display = true;
+                notify();
+                text.setVisible(false);
+            }
+        }
+		
 	}
 	class MyTask implements Runnable {
 
@@ -65,28 +89,30 @@ public class LettersUI extends Application {
 		LettersUI instance;
 		Text text;
 
-		public MyTask(Text text, LettersUI zeroOROne) {
+
+		public MyTask(Text text, LettersUI zeroOROne, ReentrantLock lock) {
 			this.text = text;
 			this.instance = zeroOROne;
+			this.lock=lock;
 		}
 
 		@Override
 		public void run() {
-			boolean show = true;
-			long startTime = System.currentTimeMillis();
-			while ((startTime + timeBudgetms) > System.currentTimeMillis()) {
-				instance.showText(text, show);
-				show = !show;
-				try {
-					Random rn = new Random();
-					int range = 3000 - 1000 + 1;
-					int randomNum = rn.nextInt(range) + 1000;
-					Thread.yield();
-					Thread.sleep(randomNum);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+				boolean show = true;
+				long startTime = System.currentTimeMillis();
+				while ((startTime + timeBudgetms) > System.currentTimeMillis()) {
+					instance.showText(text, show);
+					show = !show;
+					try {
+						Random rn = new Random();
+						int range = 3000 - 1000 + 1;
+						int randomNum = rn.nextInt(range) + 1000;
+						Thread.yield();
+						Thread.sleep(randomNum);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
-			}
 		}
 	}
 }
